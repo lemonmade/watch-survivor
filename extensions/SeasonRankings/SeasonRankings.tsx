3df294271af6f8c43b81b81dd56ad6036f63eeaf
Chris Sauve <chrismsauve@gmail.com>
2021-03-19 18:32:01 -0400

    Update season rankings

--- extensions/SeasonRankings/SeasonRankings.tsx
+++ extensions/SeasonRankings/SeasonRankings.tsx
@@ -1,5 +1,5 @@
 import {Text} from '@watching/clips-react';
 
 export function SeasonRankings() {
-  return <Text>Season rankings!</Text>
+  return <Text>Season rankings! (rendered at {new Date().toLocaleTimeString()})</Text>
 }
