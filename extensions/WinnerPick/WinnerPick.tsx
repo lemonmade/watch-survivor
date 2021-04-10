627917c2da425a39e2041894cdbcdf696c408fab
Chris Sauve <chrismsauve@gmail.com>
2021-04-10 16:02:44 -0400

    Update dependencies

--- extensions/WinnerPick/WinnerPick.tsx
+++ extensions/WinnerPick/WinnerPick.tsx
@@ -1,11 +1,14 @@
-import {View, useApi} from '@watching/clips-react';
+import {View, useApi, useConfiguration} from '@watching/clips-react';
 
 export function WinnerPick() {
+  const configuration = useConfiguration();
+
   return (
     <View>
       {Object.entries(useApi()).map(([key, value]) => <View key={key}>
         {key}: {JSON.stringify(value)}
       </View>)}
+      <View>configuration: {JSON.stringify(configuration)}</View>
     </View>
   )
 }
