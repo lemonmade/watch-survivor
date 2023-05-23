46ab15ee03d9cf7366d0859216b016369cfb6184
Chris Sauve <chrismsauve@gmail.com>
2023-05-22 22:19:40 -0400

    Add DOM example

---
+++ extensions/DomExample/SeriesAccessory.ts
@@ -0,0 +1,33 @@
+import {extension, getQuery} from '@watching/clips';
+import '@watching/clips/elements';
+import {type SeriesQueryData} from './SeriesQuery.graphql';
+
+export default extension((root, {query, target}) => {
+  const {series} = getQuery<SeriesQueryData>(query);
+
+  const seriesNameText = document.createTextNode(series.name);
+
+  query.subscribe(() => {
+    const {series} = getQuery<SeriesQueryData>(query);
+    seriesNameText.data = series.name;
+  });
+
+  const seriesText = document.createElement('ui-text');
+  seriesText.emphasis = true;
+  seriesText.append(seriesNameText);
+
+  const targetText = document.createElement('ui-text');
+  targetText.emphasis = true;
+  targetText.append(target);
+
+  const textBlock = document.createElement('ui-text-block');
+  textBlock.append(
+    'You are rendering in the ',
+    targetText,
+    ' extension point, on a series named ',
+    seriesText,
+    '!',
+  );
+
+  root.append(textBlock);
+});
