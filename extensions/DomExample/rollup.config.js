0a5eebc1be689000f161b378e82ccb560e8633d0
Chris Sauve <chrismsauve@gmail.com>
2023-05-27 21:05:17 -0400

    More dependency updates

--- extensions/DomExample/rollup.config.js
+++ extensions/DomExample/rollup.config.js
@@ -1,16 +1,12 @@
-import {extensionRollupConfiguration} from '@watching/tools/rollup';
+import {extensionRollupConfiguration} from '@watching/cli/tools/rollup';
 import svelte from 'rollup-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 
-export default extensionRollupConfiguration(
-  import.meta.url,
-  {mode: 'development'},
-  {
-    plugins: [
-      svelte({
-        emitCss: false,
-        preprocess: sveltePreprocess(),
-      }),
-    ],
-  },
-);
+export default extensionRollupConfiguration(import.meta.url, {
+  plugins: [
+    svelte({
+      emitCss: false,
+      preprocess: sveltePreprocess(),
+    }),
+  ],
+});
