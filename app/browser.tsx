94995e80fdb2704d65ee7f6ca549897a0358992d
Chris Sauve <chrismsauve@gmail.com>
2023-06-19 21:10:25 -0400

    Update watching dependencies

--- app/browser.tsx
+++ app/browser.tsx
@@ -9,11 +9,7 @@ import App from './App.tsx';
 const element = document.querySelector('#app')!;
 
 const trpcClient = trpc.createClient({
-  links: [
-    // We need to use an absolute URL so that queries will
-    // work during server-side rendering
-    httpBatchLink({url: new URL('/api', window.location.href).href}),
-  ],
+  links: [httpBatchLink({url: new URL('/api', window.location.href).href})],
 });
 
 hydrateRoot(element, <App trpc={trpcClient} />);
